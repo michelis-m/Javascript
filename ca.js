ad2817fde5bf0410de18b381e679b4cfa34922d8
michelis-m <michelis_m@yahoo.com>
2016-06-02 21:34:53 +0100

    Update ca.js

--- ca.js
+++ ca.js
@@ -40,7 +40,7 @@ console.log('Your bill is '+cashRegister.total);
 
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://www.codecademy.com/", false);
-// Add your code below!
 xhr.send();
 console.log(xhr.status);
 console.log(xhr.statusText);
+/********************************************************************************************************************************/
